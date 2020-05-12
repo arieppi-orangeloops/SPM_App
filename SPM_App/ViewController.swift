ba268492ad89d11c993382339fe0ba84d79f079a
Agustin Rieppi <arieppi@orangeloops.com>
2020-05-12 15:30:51 -0300

    Initial

---
+++ SPM_App/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SPM_App
+//
+//  Created by Agustin Rieppi on 5/12/20.
+//  Copyright © 2020 Agustin Rieppi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var packageText: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
